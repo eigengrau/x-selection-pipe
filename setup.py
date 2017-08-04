5ce99f12061550c5e129d932f7ba2ed6a2995b83
Sebastian Reuße <seb@wirrsal.net>
2017-08-04 13:46:00 +0200

    Bump pygobject dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'xselection.util'
     ],
     package_dir={'': 'src'},
-    install_requires=['pygobject >=3.16, <3.23'],
+    install_requires=['pygobject >=3.16, <3.25'],
     license="GPL3",
     entry_points={
         'console_scripts': [
