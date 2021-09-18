9c90b89232e4e354c72ee92d029195c09dd32c68
Sebastian Reuße <seb@schattenkopie.de>
2021-09-18 18:42:42 +0200

    build(nix): prefer passing dependencies explicitly

--- nix/x-selection-pipe.nix
+++ nix/x-selection-pipe.nix
@@ -1,12 +1,9 @@
-{ pkgs }:
-pkgs.python38Packages.buildPythonApplication {
+{ gobject-introspection, gtk3, python3Packages, wrapGAppsHook }:
+python3Packages.buildPythonApplication {
   pname = "x-selection-pipe";
   version = "0.1.0.0";
-  nativeBuildInputs = [ pkgs.wrapGAppsHook ];
-  propagatedBuildInputs = with pkgs; [
-    python38Packages.pygobject3
-    gtk3
-    gobject-introspection
-  ];
+  nativeBuildInputs = [ wrapGAppsHook ];
+  propagatedBuildInputs =
+    [ python3Packages.pygobject3 gtk3 gobject-introspection ];
   src = ../.;
 }
